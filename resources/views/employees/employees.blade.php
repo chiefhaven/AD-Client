3a4b49f7589b1b73d5f96f438dc6c3023f05b280
chiefhaven <chikumbahappy@gmail.com>
2025-02-24 16:57:48 +0200

    Update employees.blade.php

--- resources/views/employees/employees.blade.php
+++ resources/views/employees/employees.blade.php
@@ -125,8 +125,6 @@
 
 @push('js')
 
-<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
 <script>
 
     $(document).ready(function() {
@@ -141,9 +139,6 @@
 {{-- Add common CSS customizations --}}
 
 @push('css')
-
-
-<link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
 
 
 <style type="text/css">
